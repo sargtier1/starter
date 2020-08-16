983ef8469d52a15d46ba612850f904cca554ad04
JustTams <sarg.tier1@gmail.com>
2020-08-15 21:02:59 -0500

    simple starter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import * as React from 'react'
+import { AppProps } from 'next/app'
+import {
+  theme,
+  ThemeProvider,
+  CSSReset,
+  ColorModeProvider,
+} from '@chakra-ui/core'
+
+function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
